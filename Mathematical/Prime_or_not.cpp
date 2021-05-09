6bbf9856ad387b88c211bf3f3b5e94d80e612453
unknown <ap1079997@gmail.com>
2021-05-09 17:14:33 +0530

    Version 2

---
+++ Mathematical/Prime_or_not.cpp
@@ -0,0 +1,19 @@
+A number is Prime or not
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+
+	for(int i=2;i<n;i++){
+		if(n%i == 0){
+			cout<<"Not Prime";
+			return 0;
+		}
+	}
+
+	cout<<"Prime";
+	return 0;
+}
